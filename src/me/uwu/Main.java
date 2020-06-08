00be562b896f61ed98b5596a2c7207ea95f655ca
MrPierre20 <mrpierre007@gmail.com>
2020-06-08 22:00:50 +0200

    finished path + fixed %appdata% & %UserProfile% Now open Explorer when finished Exit on end

--- src/me/uwu/Main.java
+++ src/me/uwu/Main.java
@@ -1,28 +1,28 @@
 package me.uwu;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
-import org.apache.commons.io.FileUtils;
-
-import java.io.File;
-import java.io.IOException;
 
 public class Main extends Application {
 
+    public TextField path;
+
+    @FXML
+    private TextField userField;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("uwu.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("UwU.fxml"));
         primaryStage.setTitle("Cache Dumper by UwU#0001");
         primaryStage.setScene(new Scene(root, 1270, 710));
         primaryStage.show();
         primaryStage.resizableProperty().set(false);
+
     }
 
 
--- src/me/uwu/controllers/Controller.java
+++ src/me/uwu/controllers/Controller.java
@@ -3,11 +3,17 @@ package me.uwu.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import me.uwu.utils.FastCopy;
 import me.uwu.utils.FastDelete;
 import me.uwu.utils.FileInfo;
 import org.apache.commons.io.FileUtils;
 
+import java.awt.*;
 import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.charset.CharsetDecoder;
@@ -21,13 +27,30 @@ import java.util.Scanner;
 
 public class Controller {
 
+    public TextField path;
     private String tempPath = System.getenv("APPDATA")+"/CacheDumper/tempfiles/";
 
+    @FXML
+    private TextField userField;
+
     @FXML protected void dumpThis(ActionEvent event) throws IOException {
+        String finalPath;
+        System.out.println(path.getText());
+
+        System.out.println(System.getenv("UserProfile"));
+
+        finalPath = path.getText();
+        finalPath = finalPath.replace("%appdata%",System.getenv("APPDATA"));
+        finalPath = finalPath.replace("%UserProfile%",System.getenv("UserProfile"));
+
+        System.out.println(finalPath);
         System.out.println("\u001B[32m" + "Started DUUUUUUMPING boi !" + "\u001B[0m");
+
+
 
         System.out.println("\u001B[34m" + "Cleaning temp files" + "\u001B[0m");
         FastDelete.folder(tempPath);
+        FastDelete.folder(finalPath+"/Cache Dumper");
 
         System.out.println("\u001B[34m" + "Copy cache" + "\u001B[0m");
         FastCopy.folder(System.getenv("APPDATA")+"/discord\\Cache",tempPath);
@@ -115,7 +138,16 @@ public class Controller {
 
         }
 
+        FastCopy.folder(System.getenv("APPDATA")+"/CacheDumper/tempfiles",finalPath+"/Cache Dumper");
+
+        FastDelete.folder(tempPath);
+
         System.out.println("\u001B[32m" + "Succesfully dumped" + "\u001B[0m");
+
+        Desktop.getDesktop().open(new File(finalPath+"/Cache Dumper"));
+
+        System.exit(-1);
+
 
     }
 }
